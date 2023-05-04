e17dc99dbcd2ec838a5dc589589c3ae0742c877a
Sourav Sharma <souravgopal25@gmail.com>
2023-05-04 18:31:26 +0530

    Grpc 101 started

---
+++ grpc_101/greeting_service/src/test/java/com/example/grpcspringbootexample/GrpcSpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpcspringbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
